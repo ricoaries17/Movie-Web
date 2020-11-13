3c0509ec0230c8076e1416dfbb4e9f70f9fc3385
Rico Ariesta Wardana <ricoaries17@gmail.com>
2020-11-13 07:52:34 +0700

    menambahkan fitur detail

--- src/App.js
+++ src/App.js
@@ -10,66 +10,73 @@ class App extends Component{
     super(props)
     this.state = {}
     this.performSearch("")
+    this.TopRated()
+    this.gotoHome()
   }
 
   performSearch(searchTerm) {
+    $('#movie').attr('class', 'deactive');
     console.log("Perform search using moviedb")
-    var urlString;
-    if(searchTerm==null){
-      urlString="https://api.themoviedb.org/3/trending/movie/week?api_key=3cd63003f46b53b2f9cba3565b66ee34"
-      $.ajax({
-        url: urlString,
-        success: (searchResults) => {
-          console.log("Fetched data successfully")
-          const results = searchResults.results
-          var movieRows = []
-          results.forEach((movie) => {
-            movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
-            // console.log(movie.poster_path)
-            const movieRow = <Movie key={movie.id} movie={movie}/>
-            movieRows.push(movieRow)
-          })
-  
-          this.setState({rows: movieRows})
-        },
-        error: (xhr, status, err) => {
-          console.error("Failed to fetch data")
-        }
-      })
-    }
-    else{
-      urlString = "https://api.themoviedb.org/3/search/movie?api_key=3cd63003f46b53b2f9cba3565b66ee34&query=" + searchTerm
-      $.ajax({
-        url: urlString,
-        success: (searchResults) => {
-          console.log("Fetched data successfully")
-          const results = searchResults.results
-          var movieRows = []
-          results.forEach((movie) => {
-            movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
-            // console.log(movie.poster_path)
-            const movieRow = <Movie key={movie.id} movie={movie}/>
-            movieRows.push(movieRow)
-          })
-  
-          this.setState({rows: movieRows})
-        },
-        error: (xhr, status, err) => {
-          console.error("Failed to fetch data")
-        }
-      })
-    }
-    
-    
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=3cd63003f46b53b2f9cba3565b66ee34&query=" + searchTerm
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("Fetched data successfully")
+        const results = searchResults.results
+        var movieRows = []
+        results.forEach((movie) => {
+          movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+          // console.log(movie.poster_path)
+          const movieRow = <Movie key={movie.id} movie={movie}/>
+          movieRows.push(movieRow)
+        })
+
+        this.setState({rows: movieRows})
+      },
+      error: (xhr, status, err) => {
+        console.error("Failed to fetch data")
+      }
+    })
   }
 
-  
+  TopRated() {
+    console.log("Top Rated Movie")
+    $('#movie').attr('class', 'active');
+    const urlString = "https://api.themoviedb.org/3/trending/movie/week?api_key=3cd63003f46b53b2f9cba3565b66ee34"
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log("Fetched data successfully")
+        const results = searchResults.results
+        var movieRows = []
+
+        results.forEach((movie) => {
+          movie.poster_src = "https://image.tmdb.org/t/p/w185" + movie.poster_path
+          // console.log(movie.poster_path)
+          const movieRow = <Movie key={movie.id} movie={movie}/>
+          movieRows.push(movieRow)
+        })
+
+        this.setState({rows: movieRows})
+      },
+      error: (xhr, status, err) => {
+        console.error("Failed to fetch data")
+      }
+    })
+  }
 
   searchChangeHandler(event) {
     console.log(event.target.value)
     const boundObject = this
     const searchTerm = event.target.value
     boundObject.performSearch(searchTerm)
+  }
+  gotoHome(){
+    $('#movie').attr('class', 'active');
+    $('.card-content-deactive').attr('class', 'card-content-active');
+    $('.deactive').attr('class', 'active');
+    $('#detail').attr('class', 'deactive');
+    
   }
 
   render() {
@@ -79,12 +86,15 @@ class App extends Component{
         <nav className="NavBar">
           <div className="Logo">
             <img src={logo} height="40px"></img>
-            <h2>Theater Flix</h2>
+            <h2 id="Home" onClick={this.gotoHome}>Theater Flix</h2>
           </div>
           <input onChange={this.searchChangeHandler.bind(this)} placeholder="Enter search term"/>
         </nav>
         </header>
-       
+        <div className="deactive" id="detail"></div>
+        <div className="active" id="movie" onLoad={this.TopRated}>
+          <h2> TOP RATED MOVIES</h2>
+        </div>
         {this.state.rows}
 
       </div>
--- src/Movie.js
+++ src/Movie.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
 import './App.css';
+import $ from 'jquery'
+
+class Empty extends Component{
+    render(){
+        return
+    }
+}
 
 class Movie extends Component {
     viewMovie() {
+    $('.active').attr('class', 'deactive');
+    $('#movie').attr('class', 'deactive');
+    $('#detail').attr('class', 'active');
+    $('.card-content-active').attr('class', 'card-content-deactive');
+    
       // console.log("Trying to view movie")
       // console.log(this.props.movie.title)
-      const url = "https://www.themoviedb.org/movie/" + this.props.movie.id
-      window.location.href = url
+      const poster =this.props.movie.poster_src
+      const judul =this.props.movie.title
+      const deskripsi= this.props.movie.overview
+      const rating=this.props.movie.vote_average
+      const tanggal=this.props.movie.release_date
+      $('#detail').html(`
+      <div>
+      
+        <img alt="poster" src="`+poster+`"></img>
+        <div className="active">
+        <h3>"`+judul+`"</h3>
+        <p>"`+deskripsi+`"</p>
+        <p>Rating : `+rating+`</p>
+        <p>`+tanggal+`</p>
+
+      </div>`)
     }
   
     render() {
       return (
-          <div className="card-content">
+          <div className="card-content-active" id="list">
               <img alt="poster" width="120" src={this.props.movie.poster_src}></img>
-              <div className="Keterangan">
+              <div className="active">
                 <h3>{this.props.movie.title}</h3>
-                <p>{this.props.movie.overview}</p>
-                <input id="button" type="button" onClick={this.viewMovie.bind(this)} value="View"/>
+                <p>{this.props.movie.vote_average}</p>
+                <p>{this.props.movie.release_date}</p>
+                <input className="button" type="button" onClick={this.viewMovie.bind(this)} value="View"/>
               </div>
           </div>
       )
